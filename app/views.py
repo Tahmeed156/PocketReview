5d2df9c3ebfcc174a296dae2f8b64b9549083c59
Tahmeed Tarek <tahmeedtarek@gmail.com>
2021-01-14 00:37:11 +0600

    added django messages and list shuffling

--- app/views.py
+++ app/views.py
@@ -1,17 +1,15 @@
 import os
-from random import choice
+from random import choice, sample
 import requests
 import datetime 
 from functools import lru_cache
 
 from django.shortcuts import render, redirect
-from django.views.decorators.cache import cache_page
 from django.core.cache import cache
+from django.contrib import messages
 
 
 def index(request):
-
-    # TODO paginate or allow count selection
 
     tag_name = request.POST.get('tag_name', '')
     print(tag_name)
@@ -21,8 +19,11 @@ def index(request):
         return redirect('random')
       else:
         return redirect('random', tag_name=tag_name)
-    if request.POST.get('type') == 'refresh':
+    elif request.POST.get('type') == 'refresh':
       refresh(tag_name)
+      messages.info(request, 'article list has been updated!')
+    else:
+      pass
 
     COUNT = 5
 
@@ -35,16 +36,17 @@ def index(request):
     
     articles = []
     if article_list == []:
-      return render(request, 'index.html', {'articles': []})
+      messages.info(request, 'hit refresh to update articles & make sure you\'re using a correct tag')
+      return render(request, 'index.html', {'articles': [], 'tag_name': tag_name})
 
-    for article in list(article_list.values())[:COUNT]:
+    for article in sample(list(article_list.values()), COUNT):
       articles.append({
         'item_id': article['item_id'],
         'given_title': article['given_title'],
         'given_url': article['given_url'],
         'time_added': datetime.datetime.fromtimestamp(int(article['time_added'])).strftime('%d/%m/%Y'),
         'excerpt': article['excerpt'],
-        'time_to_read': article['time_to_read']
+        'time_to_read': article.get('time_to_read', '?')
       })
 
     return render(request, 'index.html', {
@@ -55,27 +57,18 @@ def index(request):
 
 def random(request, tag_name=''):
 
-    # TODO refresh list
-    # TODO messages
-
-    ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
-    CONSUMER_KEY = os.environ.get('CONSUMER_KEY')
     key_name = os.environ.get('ACCESS_TOKEN') + '$' + tag_name
 
     if cache.get(key_name):
       article_list = cache.get(key_name)
     else:
-      archive_link = f'https://getpocket.com/v3/get?consumer_key={CONSUMER_KEY}&access_token={ACCESS_TOKEN}&state=archive&tag={tag_name}'
-      response = requests.get(archive_link)
-      article_list = response.json()['list']
-      cache.set(key_name, article_list)
-    
-    try:
-      article = choice(list(article_list.values()))
-      return redirect(article['given_url'], permanent=True)
-    except AttributeError:
+      article_list = []
+
+    if article_list == []:
       return redirect('/')
 
+    article = choice(list(article_list.values()))
+    return redirect(article['given_url'], permanent=True)
 
 # ============== HELPERS =============
 
