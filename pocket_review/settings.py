e2ef47938d1eef0902f46cdf3e3dc5502bdf0207
Tahmeed Tarek <tahmeedtarek@gmail.com>
2021-01-14 16:52:23 +0600

    setup guide, logo

--- pocket_review/settings.py
+++ pocket_review/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a4#)l(*k!u5i6&o3#wgz*rqdq#4=0f&x$rd7qet+zu^c@26g1z'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_DEBUG')
+DEBUG = os.environ.get('DJANGO_DEBUG', '0') == '1'
 
 ALLOWED_HOSTS = []
 
